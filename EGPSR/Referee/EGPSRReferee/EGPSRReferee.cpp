e52568372a0c6686331fcf5b1a268016d1fc227f
Hagiwara Yoshinobu <yhagiwara@nii.ac.jp>
2014-02-26 17:11:01 +0900

    Add all files for EGPSR

---
+++ EGPSR/Referee/EGPSRReferee/EGPSRReferee.cpp
@@ -0,0 +1,21 @@
+// EGPSRReferee.cpp : main project file.
+
+#include "stdafx.h"
+#include "Form1.h"
+#include "EGPSRReferee.h"
+
+using namespace EGPSRReferee;
+
+[STAThreadAttribute]
+int main(array<System::String ^> ^args)
+{
+	// Enabling Windows XP visual effects before any controls are created
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false); 
+
+	// Create the main window and run it
+	Form1 ^mform = gcnew Form1();
+	Application::Run(mform);
+
+	return 0;
+}
