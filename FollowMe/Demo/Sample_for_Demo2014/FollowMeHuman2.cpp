439e57b89299e0d0880666c4ef692554b40d6fab
Yoshiaki Mizuchi <mi.good.bright@gmail.com>
2014-03-04 19:01:15 +0900

    Modify FollowMe/Demo/Sample_for_Demo

--- FollowMe/Demo/Sample_for_Demo2014/FollowMeHuman2.cpp
+++ FollowMe/Demo/Sample_for_Demo2014/FollowMeHuman2.cpp
@@ -12,207 +12,111 @@ bool start;
 bool sw;
 using namespace std;
 
-class MyController : public Controller {  
-public:  
-  void onInit(InitEvent &evt);  
-  double onAction(ActionEvent&);  
-  void onRecvMsg(RecvMsgEvent &evt); 
-  void onCollision(CollisionEvent &evt); 
+class MyController : public Controller {
+public:
+	void onInit(InitEvent &evt);
+	double onAction(ActionEvent&);
+	void onRecvMsg(RecvMsgEvent &evt);
+	void onCollision(CollisionEvent &evt);
 
-  /* @brief  位置を指定しその方向に回転を開始し、回転終了時間を返します
-   * @param  pos 回転したい方向の位置
-   * @param  vel 回転速度
-   * @param  now 現在時間
-   * @return 回転終了時間
-   */
-  double rotateTowardObj1(Vector3d pos, double vel, double now);
-  double rotateTowardObj2(Vector3d pos, double vel, double now);
-  //avoid
-  double rotateTowardObj3(Vector3d pos, double vel, double now);
+	/* @brief  位置を指定しその方向に回転を開始し、回転終了時間を返します
+	* @param  pos 回転したい方向の位置
+	* @param  vel 回転速度
+	* @param  now 現在時間
+	* @return 回転終了時間
+	*/
+	double rotateTowardObj1(Vector3d pos, double vel, double now);
+	double rotateTowardObj2(Vector3d pos, double vel, double now);
+	//avoid
+	double rotateTowardObj3(Vector3d pos, double vel, double now);
 
-  /* @brief  位置を指定しその方向に進みます
-   * @param  pos   行きたい場所
-   * @param  vel   移動速度
-   * @param  range 半径range以内まで移動
-   * @param  now   現在時間
-   * @return 到着時間
-   */
-  double goToObj(Vector3d pos, double vel, double range, double now);
+	/* @brief  位置を指定しその方向に進みます
+	* @param  pos   行きたい場所
+	* @param  vel   移動速度
+	* @param  range 半径range以内まで移動
+	* @param  now   現在時間
+	* @return 到着時間
+	*/
+	double goToObj(Vector3d pos, double vel, double range, double now);
 
 private:
- SimObj *my;
-// ViewService *m_view;
+	SimObj *my;
+	// ViewService *m_view;
 
-};  
+};
 
-void MyController::onInit(InitEvent &evt) 
+void MyController::onInit(InitEvent &evt)
 {
-  my = getObj(myname()); 
+	my = getObj(myname());
 
+	start = false;
+	sw = false;
+	// 左手を下に下げます  
+	my->setJointAngle("LARM_JOINT2", DEG2RAD(-90));
 
-  start =false;
-  sw=false;
-      // 左手を下に下げます  
-      my->setJointAngle("LARM_JOINT2", DEG2RAD(-90));  
-  
-      // 右手をsageます  
-      my->setJointAngle("RARM_JOINT2", DEG2RAD(90));
+	// 右手をsageます  
+	my->setJointAngle("RARM_JOINT2", DEG2RAD(90));
 
-/*
-      my->setJointAngle("LLEG_JOINT2", DEG2RAD(-90));  
-      my->setJointAngle("RLEG_JOINT2", DEG2RAD(-90));
+}
 
-
-      my->setJointAngle("LLEG_JOINT4", DEG2RAD(90));  
-      my->setJointAngle("RLEG_JOINT4", DEG2RAD(90));
-*/
-
-
-
-
-
-//  m_view = (ViewService*)connectToService("SIGViewer");  
-}  
-  
 double MyController::onAction(ActionEvent &evt)
-{  
-/*
-// カメラの垂直方向(radian)の視野角を取得します  
-  double fovy = my->getCamFOV(5) * PI / 180.0;  
-  
-  // アスペクト比を取得します  
-  double ar = my->getCamAS(5);  
-    
-  // カメラの水平方向の視野角(degree)を計算します  
-  double fovx = 2 * atan(tan(fovy*0.5)*ar) * 180.0 / PI;  
-  
-  unsigned char distance = 255;
-  unsigned char least_distance = 255;  
-// 視線方向からの角度  
-    double theta = 0.0;  
+{
+	int count = 0;
+	Vector3d pos;
 
-    // 視線方向からの水平面の角度  
-    double phi = 0.0;
+	if (start == true){
+		while (count<20){
+			if (sw == false){
+				my->getPosition(pos);
+				my->setPosition(pos.x(), pos.y(), pos.z() - 10);
+				my->setJointAngle("LARM_JOINT1", DEG2RAD(-30));
+				my->setJointAngle("RLEG_JOINT2", DEG2RAD(-20));
+				my->setJointAngle("RLEG_JOINT4", DEG2RAD(10));
+				usleep(100000);
+				my->setJointAngle("LARM_JOINT1", DEG2RAD(0));
+				my->setJointAngle("RLEG_JOINT2", DEG2RAD(0));
+				my->setJointAngle("RLEG_JOINT4", DEG2RAD(0));
+				sw = true;
+			}
+			else{
+				my->getPosition(pos);
+				my->setPosition(pos.x(), pos.y(), pos.z() - 10);
+				my->setJointAngle("RARM_JOINT1", DEG2RAD(-30));
+				my->setJointAngle("LLEG_JOINT2", DEG2RAD(-20));
+				my->setJointAngle("LLEG_JOINT4", DEG2RAD(10));
+				usleep(100000);
+				my->setJointAngle("RARM_JOINT1", DEG2RAD(0));
+				my->setJointAngle("LLEG_JOINT2", DEG2RAD(0));
+				my->setJointAngle("LLEG_JOINT4", DEG2RAD(0));
+				sw = false;
+			}
+			count++;
 
-    double phi2 = 0.0;  
-  
-  if(m_view != NULL) {    
-  
-    // 視線方向水平面の距離データを取得します    
-    ViewImage *img = m_view->distanceSensor1D(0,255,5);    
-    char *buf = img->getBuffer();  
+			if(count==7){
+				usleep(3000000);
+			}
 
-    // 距離データ画像の幅取得  
-    int width = img->getWidth();  
-  
-    // 距離データ画像の高さ取得  
-    int height = img->getHeight();   
-    
-    // データの長さを取得します     
- //   int length = img->getBufferLength();    
-  
-    
-    // 距離データの最小値を求めます    
-    for(int i = 0; i < width; i++){    
-      for(int j = 0; j < height; j++){  
- 	   int index = j *width + i;  
- 	   unsigned char tmp_distance = (unsigned char)buf[index];  
-  	  if(tmp_distance < least_distance){  
-  
-    	  // 水平方向における視線方向からの角度(rad)を計算します  
-    	  phi   = fovx*i/(width-1.0) - fovx/2.0;  
+			start = false;
+		}
+	}
 
-  	  least_distance = buf[i]; 
-	  }   
-      }    
-    } 
-    LOG_MSG(("phi = %.1f,distance = %d",phi,least_distance));    
-    delete img;   
-   }
+	return 0.1;
+}
 
-*/
-
-/*
-    //自分の位置を得ます。  
-    Vector3d pos;  
-    my->getPosition(pos);  
-  
-    //y軸周りの自分の回転を得ます。（クオータニオン）  
-    Rotation rot;  
-    my->getRotation(rot);  
-  
-    //クオータニオンから回転角を導出します。  
-    double theta = 2*asin(rot.qy());  
-  
-    double dx = 0;  
-    double dz = 0;  
-  
-    //移動する方向を決定します。  
-    dx = sin(theta) * vel;  
-    dz = cos(theta) * vel;  
-*/  
-
-
-/* 手をフル
-    my->setJointAngle("RARM_JOINT2", DEG2RAD(-60)); 
-    usleep(500000);
-    my->setJointAngle("RARM_JOINT2", DEG2RAD(-40));
-*/
-
-
-
-
-    int count=0;
-    Vector3d pos;
-
-
-    if(start==true){
-                while(count<20){
-                    if(sw == false){
-                        my->getPosition(pos);
-                        my->setPosition(pos.x(),pos.y(),pos.z()-10);
-                        my->setJointAngle("LARM_JOINT1", DEG2RAD(-30)); 
-                        my->setJointAngle("RLEG_JOINT2", DEG2RAD(-20));
-                        my->setJointAngle("RLEG_JOINT4", DEG2RAD(10));   
-                        usleep(100000);
-                        my->setJointAngle("LARM_JOINT1", DEG2RAD(0));  
-                        my->setJointAngle("RLEG_JOINT2", DEG2RAD(0)); 
-                        my->setJointAngle("RLEG_JOINT4", DEG2RAD(0)); 
-                        sw = true;
-                    }else{
-                        my->getPosition(pos);
-                        my->setPosition(pos.x(),pos.y(),pos.z()-10);
-                        my->setJointAngle("RARM_JOINT1", DEG2RAD(-30)); 
-                        my->setJointAngle("LLEG_JOINT2", DEG2RAD(-20));  
-                        my->setJointAngle("LLEG_JOINT4", DEG2RAD(10));  
-                        usleep(100000);
-                        my->setJointAngle("RARM_JOINT1", DEG2RAD(0));  
-                        my->setJointAngle("LLEG_JOINT2", DEG2RAD(0)); 
-                        my->setJointAngle("LLEG_JOINT4", DEG2RAD(0));  
-                        sw = false;
-                    }
-                    count++;
-                    start=false;
-               }
-    }
-
-    return 0.1;      
-}   
-  
 void MyController::onRecvMsg(RecvMsgEvent &evt)
-{  
-    string msg = evt.getMsg();
-    if(msg == "point1"){
-        start = true;
-    }
-}  
+{
+	string msg = evt.getMsg();
+	if (msg == "point1"){
+		start = true;
+	}
+}
 
-void MyController::onCollision(CollisionEvent &evt) 
+void MyController::onCollision(CollisionEvent &evt)
 {
 
 }
-  
-extern "C" Controller * createController() {  
-  return new MyController;  
-}  
+
+extern "C" Controller * createController() {
+	return new MyController;
+}
 
--- FollowMe/Demo/Sample_for_Demo2014/FollowReferee_v2.cpp
+++ FollowMe/Demo/Sample_for_Demo2014/FollowReferee_v2.cpp
@@ -10,11 +10,12 @@
 
 //角度からラジアンに変換します
 #define DEG2RAD(DEG) ( (PI) * (DEG) / 180.0 )
+
 struct coordinate{
-    double x[255];
-    double y[255];
-    double z[255];
-    double w[255];
+	double x[255];
+	double y[255];
+	double z[255];
+	double w[255];
 };
 
 
@@ -33,313 +34,315 @@ bool elevator_end;
 double dx,dy,dz;
 double x,y,z,w;
 int i; 
-coordinate temp; 
+
 
 
 
 using namespace std;
 
 class MyController : public Controller {  
-public:  
-  void onInit(InitEvent &evt);  
-  double onAction(ActionEvent&);  
-  void onRecvMsg(RecvMsgEvent &evt); 
-  void onCollision(CollisionEvent &evt); 
+public:
+	void onInit(InitEvent &evt);  
+	double onAction(ActionEvent&);  
+	void onRecvMsg(RecvMsgEvent &evt); 
+	void onCollision(CollisionEvent &evt); 
 
-  /* @brief  位置を指定しその方向に回転を開始し、回転終了時間を返します
-   * @param  pos 回転したい方向の位置
-   * @param  vel 回転速度
-   * @param  now 現在時間
-   * @return 回転終了時間
-   */
-  double rotateTowardObj(Vector3d pos);
+	/* @brief  位置を指定しその方向に回転を開始し、回転終了時間を返します
+	 * @param  pos 回転したい方向の位置
+	 * @param  vel 回転速度
+	 * @param  now 現在時間
+	 * @return 回転終了時間
+	 */
+	double rotateTowardObj(Vector3d pos);
 
-  /* @brief  位置を指定しその方向に進みます
-   * @param  pos   行きたい場所
-   * @param  vel   移動速度
-   * @param  range 半径range以内まで移動
-   * @param  now   現在時間
-   * @return 到着時間
-   */
-  double goToObj(Vector3d pos, double vel, double range, double now);
+	/* @brief  位置を指定しその方向に進みます
+	 * @param  pos   行きたい場所
+	 * @param  vel   移動速度
+	 * @param  range 半径range以内まで移動
+	 * @param  now   現在時間
+	 * @return 到着時間
+	 */
+	double goToObj(Vector3d pos, double vel, double range, double now);
 
 private:
- SimObj *my;
- std::vector<std::string> m_entities;
+	SimObj *my;
+	std::vector<std::string> m_entities;
 
-};  
+	coordinate temp; 
+};
 
 void MyController::onInit(InitEvent &evt) 
 {
-      start = false;
-      elevator = false;
-      crowd = false;
-      end = false;
-      stop = false;
-      flg1 = false;
-      flg2 = false;
-      flg3 = false;
-      first = false;
-      ok=false;
-      elevator_end = false;
-      i=0;
-   
-      my = getObj(myname());
+	start = false;
+	elevator = false;
+	crowd = false;
+	end = false;
+	stop = false;
+	flg1 = false;
+	flg2 = false;
+	flg3 = false;
+	first = false;
+	ok=false;
+	elevator_end = false;
+	i=0;
+
+	my = getObj(myname());
  
-      // 左手を下に下げます  
-      my->setJointAngle("LARM_JOINT2", DEG2RAD(-90));  
-  
-      // 右手を下に下げます  
-      my->setJointAngle("RARM_JOINT2", DEG2RAD(90));  
-      
+	// 左手を下に下げます  
+	my->setJointAngle("LARM_JOINT2", DEG2RAD(-90));  
 
-      getAllEntities(m_entities);
-}  
-  
+	// 右手を下に下げます  
+	my->setJointAngle("RARM_JOINT2", DEG2RAD(90));  
+
+	if(first==false){
+		FILE* fp;
+		x=0;
+		y=0;
+		z=0;
+		w=0; //チェックポイント
+
+		dx=0;
+		dy=0;
+		dz=0;
+
+		if((fp = fopen("node.txt", "r")) == NULL) {
+			printf("File do not exist.\n");
+			exit(0);
+		}
+		while(fscanf(fp, "%lf,%lf,%lf,%lf", &x, &y, &z,&w) != EOF) {
+			temp.x[i]=x;
+			temp.y[i]=y;
+			temp.z[i]=z;
+			temp.w[i]=w;
+			i++;
+		}
+		fclose(fp);
+		first = true;
+		i=0;
+	}
+
+	getAllEntities(m_entities);
+
+}
+
 double MyController::onAction(ActionEvent &evt)
-{  
-    int count=0;
-    int count2=0; //何歩歩くか
-    double r=0.0; //2点間の直線距離
-    double r2=0.0;
-    bool sw=false; //歩行の左右切り替え  
-    int j=0;
+{
+	int    count = 0;
+	int    count2 = 0; //何歩歩くか
+	double r = 0.0;    //2点間の直線距離
+	double r2 = 0.0;
+	bool   sw = false; //歩行の左右切り替え  
+	int    j = 0;
 
-    if(end==false){
-        if(start==true){
+	if(end==false){
+		if(start==true){
 
-            Vector3d pos;
-            Vector3d npos;
-            double angle;
-
-            if(first==false){
-                FILE* fp;           
-                x=0;
-                y=0;
-                z=0;
-                w=0; //チェックポイント
-      
-                dx=0;
-                dy=0;
-                dz=0;
-
-                if((fp = fopen("node.txt", "r")) == NULL) {
-	             printf("File do not exist.\n");
-	             exit(0);
-                }
-                while(fscanf(fp, "%lf,%lf,%lf,%lf", &x, &y, &z,&w) != EOF) {
-                    temp.x[i]=x;
-                    temp.y[i]=y;
-                    temp.z[i]=z;
-                    temp.w[i]=w;
-                    i++;
-                }
-                fclose(fp);
-                first = true;
-                i=0;
-            }
-
-            if(stop==false){
-                my->getPosition(pos);
-
-                npos.x(temp.x[i]); 
-                npos.y(temp.y[i]); 
-                npos.z(temp.z[i]); 
+			Vector3d pos;
+			Vector3d npos;
+			double angle;
 
 
+			if(stop==false){
+				my->getPosition(pos);
 
-                dx=(temp.x[i]-pos.x());
-                dy=(temp.y[i]-pos.y());
-                dz=(temp.z[i]-pos.z());
+				npos.x(temp.x[i]); 
+				npos.y(temp.y[i]); 
+				npos.z(temp.z[i]); 
 
-                angle = rotateTowardObj(npos);   
+				dx=(temp.x[i]-pos.x());
+				dy=(temp.y[i]-pos.y());
+				dz=(temp.z[i]-pos.z());
 
-                if(angle < 0.0){
-                    angle = -1.0 * angle;
-                }
+				angle = rotateTowardObj(npos);
 
-                my->setAxisAndAngle(0,1.0, 0, -angle);
+				if(angle < 0.0){
+					angle = -1.0 * angle;
+				}
 
+				my->setAxisAndAngle(0,1.0, 0, -angle);
 
-                r=sqrt(pow(dx,2)+pow(dz,2));
+				r=sqrt(pow(dx,2)+pow(dz,2));
 
-                count2 = (int)r / 10;
+				count2 = (int)r / 10;
 
-                dx = dx/count2;
-                dz = dz/count2;
+				dx = dx/count2;
+				dz = dz/count2;
 
-                while(count<count2){
-                    if(sw == false){
-                        my->getPosition(pos);
-                        my->setPosition(pos.x()+dx,pos.y(),pos.z()+dz);
-                        my->setJointAngle("LARM_JOINT1", DEG2RAD(-30)); 
-                        my->setJointAngle("RLEG_JOINT2", DEG2RAD(-20));
-                        my->setJointAngle("RLEG_JOINT4", DEG2RAD(10));   
-                        usleep(100000);
-                        my->setJointAngle("LARM_JOINT1", DEG2RAD(0));  
-                        my->setJointAngle("RLEG_JOINT2", DEG2RAD(0)); 
-                        my->setJointAngle("RLEG_JOINT4", DEG2RAD(0)); 
-                        sw = true;
-                    }else{
-                        my->getPosition(pos);
-                        my->setPosition(pos.x()+dx,pos.y(),pos.z()+dz);
-                        my->setJointAngle("RARM_JOINT1", DEG2RAD(-30)); 
-                        my->setJointAngle("LLEG_JOINT2", DEG2RAD(-20));  
-                        my->setJointAngle("LLEG_JOINT4", DEG2RAD(10));  
-                        usleep(100000);
-                        my->setJointAngle("RARM_JOINT1", DEG2RAD(0));  
-                        my->setJointAngle("LLEG_JOINT2", DEG2RAD(0)); 
-                        my->setJointAngle("LLEG_JOINT4", DEG2RAD(0));  
-                        sw = false;
-                    }
-                    count++;
-                }
-            
-                count = 0;
-                count2= 0;
-                i++;
-            }//stopのところ
+				while(count<count2){
+					if(sw == false){
+						my->getPosition(pos);
+						my->setPosition(pos.x()+dx,pos.y(),pos.z()+dz);
+						my->setJointAngle("LARM_JOINT1", DEG2RAD(-30)); 
+						my->setJointAngle("RLEG_JOINT2", DEG2RAD(-20));
+						my->setJointAngle("RLEG_JOINT4", DEG2RAD(10));   
+						usleep(100000);
+						my->setJointAngle("LARM_JOINT1", DEG2RAD(0));  
+						my->setJointAngle("RLEG_JOINT2", DEG2RAD(0)); 
+						my->setJointAngle("RLEG_JOINT4", DEG2RAD(0)); 
+						sw = true;
+					}else{
+						my->getPosition(pos);
+						my->setPosition(pos.x()+dx,pos.y(),pos.z()+dz);
+						my->setJointAngle("RARM_JOINT1", DEG2RAD(-30)); 
+						my->setJointAngle("LLEG_JOINT2", DEG2RAD(-20));  
+						my->setJointAngle("LLEG_JOINT4", DEG2RAD(10));  
+						usleep(100000);
+						my->setJointAngle("RARM_JOINT1", DEG2RAD(0));  
+						my->setJointAngle("LLEG_JOINT2", DEG2RAD(0)); 
+						my->setJointAngle("LLEG_JOINT4", DEG2RAD(0));  
+						sw = false;
+					}
+					count++;
+				}
 
-            Vector3d myPos;
-            my->getPosition(myPos);
-  
-            int entSize = m_entities.size();
-            for(int j = 0; j < entSize; j++){
-              if(m_entities[j] == "robot_004"){
-                // エンティティ取得
-                SimObj *ent = getObj(m_entities[j].c_str());
+				count = 0;
+				count2= 0;
+				i++;
+			}//stopのところ
 
-                // 位置取得
-                Vector3d tpos;
-                ent->getPosition(tpos);
+			Vector3d myPos;
+			my->getPosition(myPos);
 
-                Vector3d vec(tpos.x()-myPos.x(), tpos.y()-myPos.y(), tpos.z()-myPos.z());
+			int entSize = m_entities.size();
+			for(int j = 0; j < entSize; j++){
+				if(m_entities[j] == "robot_004"){
+					// エンティティ取得
+					SimObj *ent = getObj(m_entities[j].c_str());
 
-                r2=sqrt((vec.x()*vec.x())+(vec.z()*vec.z()));
+					// 位置取得
+					Vector3d tpos;
+					ent->getPosition(tpos);
 
-                if(r2<=200){
-                    stop=false;                
+					Vector3d vec(tpos.x()-myPos.x(), tpos.y()-myPos.y(), tpos.z()-myPos.z());
 
-                    if(temp.w[i-1] == 1.0){
-                        std::string msg = "point1";  
-                        //"man_001"にメッセージを送信します  
-                        sendMsg("man_001", msg);
-                        elevator =false; 
-                    }else if(temp.w[i-1] == 2.0){
-                        std::string msg3 = "check1";
-                        sendMsg("score",msg3);
-                        elevator = false;
-                    }else if(temp.w[i-1] == 3.0){
-                        stop=true;
-                        if(flg1==false){
-                            //broadcastMsgToSrv("Elevator");
-                            std::string msg1 = "elevator_close";
-                            sendMsg("wall_008", msg1);
-                            flg1=true;
-                            elevator = false;
-                        } 
-                        if(elevator==true){
-                            if(flg2==false){
-                               std::string msg2 = "elevator_open";
-                               sendMsg("wall_008", msg2);
-                               std::string msg4 = "elevator";
-                               sendMsg("score", msg4);
-                               flg2=true;
-                            }
-                            if(ok==true){
-                                elevator = false;
-                                stop=false;
-                            }
-                        }
-                    }else if(temp.w[i-1]==4.0){
-                               std::string msg5 = "crowd";
-                               sendMsg("score", msg5);
-                               elevator = false;
-                    }
-                }else if(r2>200){
-                    if(ok==true && flg3==false){
-                        stop=false;
-                        flg3=true;
-                        elevator = false;
-                    }else{
-                        stop = true;
-                        elevator = false;
-                    }   
-                }
-              }
-            }
-        }
-    }
-    return 0.1;      
-}   
+					r2=sqrt((vec.x()*vec.x())+(vec.z()*vec.z()));
+
+					if(r2<=200){
+						stop=false;
+
+						if(temp.w[i-1] == 1.0){
+							std::string msg = "point1";  
+							//"man_001"にメッセージを送信します  
+							sendMsg("man_001", msg);
+							elevator =false; 
+						}else if(temp.w[i-1] == 2.0){
+							std::string msg3 = "check1";
+							sendMsg("score",msg3);
+							elevator = false;
+						}else if(temp.w[i-1] == 3.0){
+							stop=true;
+							if(flg1==false){
+								//broadcastMsgToSrv("Elevator");
+								std::string msg1 = "elevator_close";
+								sendMsg("wall_008", msg1);
+								flg1=true;
+								elevator = false;
+							} 
+							if(elevator==true){
+								if(flg2==false){
+									std::string msg2 = "elevator_open";
+									sendMsg("wall_008", msg2);
+									std::string msg4 = "elevator";
+									sendMsg("score", msg4);
+									flg2=true;
+								}
+								if(ok==true){
+									elevator = false;
+									stop=false;
+								}
+							}
+						}else if(temp.w[i-1]==4.0){
+							std::string msg5 = "crowd";
+							sendMsg("score", msg5);
+							elevator = false;
+						}else if(temp.w[i-1]==5.0){
+							end=true;
+						}
+					}else if(r2>200){
+						if(ok==true && flg3==false){
+							stop=false;
+							flg3=true;
+							elevator = false;
+						}else{
+							stop = true;
+							elevator = false;
+						}
+					}
+				}
+			}
+		}
+	}
+	return 0.1;
+}
 
 
 double MyController::rotateTowardObj(Vector3d pos)
 {
-  // 自分の位置の取得
-  Vector3d myPos;
-  my->getPosition(myPos);
+	// 自分の位置の取得
+	Vector3d myPos;
+	my->getPosition(myPos);
 
-  // 自分の位置からターゲットを結ぶベクトル
-  Vector3d tmpp = pos;
-  tmpp -= myPos;
-  
-  // y方向は考えない
-  tmpp.y(0);
-  
-  // 自分の回転を得る
-  Rotation myRot;
-  my->getRotation(myRot);
+	// 自分の位置からターゲットを結ぶベクトル
+	Vector3d tmpp = pos;
+	tmpp -= myPos;
 
-  // エンティティの初期方向
-  Vector3d iniVec(0.0, 0.0, 1.0);
-      
-  // y軸の回転角度を得る(x,z方向の回転は無いと仮定)
-  double qw = myRot.qw();
-  double qy = myRot.qy();
-      
-  double theta = 2*acos(fabs(qw));
+	// y方向は考えない
+	tmpp.y(0);
 
-  if(qw*qy < 0)
-    theta = -1*theta;
+	// 自分の回転を得る
+	Rotation myRot;
+	my->getRotation(myRot);
 
-  // z方向からの角度
-  double tmp = tmpp.angle(Vector3d(0.0, 0.0, 1.0));
-  double targetAngle = acos(tmp);
+	// エンティティの初期方向
+	Vector3d iniVec(0.0, 0.0, 1.0);
 
-  // 方向
-  if(tmpp.x() > 0) targetAngle = -1*targetAngle;
-  targetAngle += theta;
+	// y軸の回転角度を得る(x,z方向の回転は無いと仮定)
+	double qw = myRot.qw();
+	double qy = myRot.qy();
 
-  if(targetAngle == 0.0){
-    return 0.0;
-  }
-  return targetAngle;
+	double theta = 2*acos(fabs(qw));
+
+	if(qw*qy < 0)
+	theta = -1*theta;
+
+	// z方向からの角度
+	double tmp = tmpp.angle(Vector3d(0.0, 0.0, 1.0));
+	double targetAngle = acos(tmp);
+
+	// 方向
+	if(tmpp.x() > 0) targetAngle = -1*targetAngle;
+	targetAngle += theta;
+
+	if(targetAngle == 0.0){
+	return 0.0;
+	}
+	return targetAngle;
 }
 
 
 void MyController::onRecvMsg(RecvMsgEvent &evt)
-{  
-    string msg = evt.getMsg();
-    if(msg == "start"){
-        start = true;
-    }else if(msg == "elevator"){
-        std::string msg0 ="elevator";
-        sendMsg("score",msg0);
-        elevator = true;
-    }else if(msg == "Collision"){
-        end = true;
-    }else if(msg == "ok"){
-        ok = true;
-    }
-}  
+{
+	string msg = evt.getMsg();
+	if(msg == "start"){
+		start = true;
+	}else if(msg == "elevator"){
+		std::string msg0 ="elevator";
+		//sendMsg("score",msg0);
+		elevator = true;
+	}else if(msg == "Collision"){
+		//end = true;
+	}else if(msg == "ok"){
+		ok = true;
+	}
+}
 
-void MyController::onCollision(CollisionEvent &evt) 
+void MyController::onCollision(CollisionEvent &evt)
 {
 
 }
-  
-extern "C" Controller * createController() {  
-  return new MyController;  
-}  
+
+extern "C" Controller * createController() {
+	return new MyController;  
+}
+
 
