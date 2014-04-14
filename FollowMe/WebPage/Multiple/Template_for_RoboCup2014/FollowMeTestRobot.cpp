174c3391676ab53133b729a706bcd71b95214974
Hagiwara Yoshinobu <yhagiwara@nii.ac.jp>
2014-04-14 16:54:51 +0900

    Add FollowMe/WebPage/Multiple/

---
+++ FollowMe/WebPage/Multiple/Template_for_RoboCup2014/FollowMeTestRobot.cpp
@@ -0,0 +1,461 @@
+#include "ControllerEvent.h"  
+#include "Controller.h"  
+#include "Logger.h"  
+#include <algorithm>
+#include <string> 
+#include <iostream>
+#include <math.h>
+#include <unistd.h>
+
+// Convert degree to radian
+#define DEG2RAD(DEG) ( (M_PI) * (DEG) / 180.0 )
+
+using namespace std;
+
+
+class MyController : public Controller
+{
+public:
+	void onInit(InitEvent &evt);
+	double onAction(ActionEvent&);
+	void onRecvMsg(RecvMsgEvent &evt);
+	void onCollision(CollisionEvent &evt);
+
+	/**
+	 * @brief  Start rotating toword target position and return rotation time.
+	 * @param  pos target position
+	 * @param  vel velocity
+	 * @param  now current time
+	 * @return end time of rotation
+	 */
+	double rotateTowardObj(Vector3d pos, double vel, double now);
+
+	/**
+	 * @brief  Move to target position
+	 * @param  pos   target position
+	 * @param  vel   velocity
+	 * @param  range allowable error range
+	 * @param  now   current time
+	 * @return end time of rotation
+	 */
+	double goToObj(Vector3d pos, double vel, double range, double now);
+
+private:
+	RobotObj *m_my;
+
+	/**
+	 * State of robot
+	 *   0  wait
+	 * 100+ 1st section
+	 * 200+ 2nd section
+	 * 300+ 3rd section
+	 * 900+ finish line
+	 */
+	int m_state;
+
+	double m_vel;      // angular velocity of wheel
+	double m_radius;   // radius of wheel
+	double m_distance; // distance of wheel
+
+	double m_time; // time to stop moving
+};
+
+void MyController::onInit(InitEvent &evt) 
+{
+	m_my = getRobotObj(myname());
+
+	// radius and distance of wheel
+	m_radius = 10.0;
+	m_distance = 10.0;
+
+	// time to stop moving
+	m_time = 0.0;
+
+	// set radius and distance of wheel
+	m_my->setWheel(m_radius, m_distance);
+
+	// angular velocity of wheel
+	m_vel = 0.3;
+
+	// state of robot
+	m_state = 0;
+}
+
+double MyController::onAction(ActionEvent &evt)
+{
+	switch(m_state){
+		// wait
+		case 0:{
+			break;
+		}
+		// 1st section
+		case 100: {
+			m_time = rotateTowardObj(Vector3d(-50,60,500),m_vel,evt.time());
+			m_state = 101;
+			break;
+		}
+		case 101: {
+			if(evt.time() >= m_time){
+				m_my->setWheelVelocity(0.0, 0.0);
+				m_time = goToObj(Vector3d(-50,60,500), m_vel*20, 1.0, evt.time());
+				m_state = 102;
+			}
+			break;
+		}
+		case 102: {
+			if(evt.time() >= m_time){
+				m_my->setWheelVelocity(0.0, 0.0);
+				m_time = goToObj(Vector3d(-50,60,575), m_vel*20, 1.0, evt.time());
+				m_state = 103;
+			}
+			break;
+		}
+		case 103: {
+			if(evt.time() >= m_time){
+				m_my->setWheelVelocity(0.0, 0.0);
+				m_time = rotateTowardObj(Vector3d(-450,60,575),m_vel,evt.time());
+				m_state = 104;
+				break;
+			}
+		}
+		case 104: {
+			if(evt.time() >= m_time){
+				m_my->setWheelVelocity(0.0, 0.0);
+				m_time = goToObj(Vector3d(-450,60,575), m_vel*20, 1.0, evt.time());
+				m_state = 105;
+			}
+			break;
+		}
+		case 105: {
+			if(evt.time() >= m_time){
+				m_my->setWheelVelocity(0.0, 0.0);
+				// wait for a walking person
+				usleep(3200000);
+				m_state = 106;
+			}
+			break;
+		}
+		case 106: {
+			m_time = rotateTowardObj(Vector3d(-525,60,575),m_vel,evt.time());
+			m_state = 107;
+			break;
+		}
+		case 107: {
+			if(evt.time() >= m_time){
+				m_my->setWheelVelocity(0.0, 0.0);
+				m_time = goToObj(Vector3d(-525,60,575), m_vel*20, 1.0, evt.time());
+				m_state = 200;
+			}
+			break;
+		}
+		// 2nd section
+		case 200: {
+			if(evt.time() >= m_time){
+				m_my->setWheelVelocity(0.0, 0.0);
+				m_time = rotateTowardObj(Vector3d(-800,60,575),m_vel,evt.time());
+				m_state = 201;
+			}
+			break;
+		}
+		case 201: {
+			if(evt.time() >= m_time){
+				m_my->setWheelVelocity(0.0, 0.0);
+				m_time = goToObj(Vector3d(-800,60,575), m_vel*20, 1.0, evt.time());
+				m_state = 202;
+			}
+			break;
+		}
+		case 202: {
+			if(evt.time() >= m_time){
+				m_my->setWheelVelocity(0.0, 0.0);
+				m_time = rotateTowardObj(Vector3d(-800,60,275),m_vel,evt.time());
+				m_state = 203;
+			}
+			break;
+		}
+		case 203: {
+			if(evt.time() >= m_time){
+				m_my->setWheelVelocity(0.0, 0.0);
+				m_time = goToObj(Vector3d(-800,60,275), m_vel*20, 1.0, evt.time());
+				m_state = 204;
+			}
+			break;
+		}
+		case 204: {
+			if(evt.time() >= m_time){
+				m_my->setWheelVelocity(0.0, 0.0);
+				m_time = rotateTowardObj(Vector3d(-1100,60,275),m_vel,evt.time());
+				m_state = 205;
+			}
+			break;
+		}
+		case 205: {
+			if(evt.time() >= m_time){
+				m_my->setWheelVelocity(0.0, 0.0);
+				m_time = goToObj(Vector3d(-1100,60,275), m_vel*20, 1.0, evt.time());
+				m_state = 206;
+			}
+			break;
+		}
+		case 206: {
+			if(evt.time() >= m_time){
+				m_my->setWheelVelocity(0.0, 0.0);
+
+				// tell robot entered in an elevator
+				broadcastMsg("entered");
+
+				//  wait for a while until a door opened
+				//sleep(5);
+				m_state = 207;
+			}
+			break;
+		}
+		case 207: {
+			break;
+		}
+		case 208: {
+			if(evt.time() >= m_time){
+				m_my->setWheelVelocity(0.0, 0.0);
+				m_time = rotateTowardObj(Vector3d(-800,60,275),m_vel,evt.time());
+				m_state = 209;
+			}
+			break;
+		}
+		case 209: {
+			if(evt.time() >= m_time){
+				m_my->setWheelVelocity(0.0, 0.0);
+				m_time = goToObj(Vector3d(-800,60,275), m_vel*20, 1.0, evt.time());
+				m_state = 210;
+			}
+			break;
+		}
+		case 210: {
+			if(evt.time() >= m_time){
+				m_my->setWheelVelocity(0.0, 0.0);
+
+				// 
+				std::string msg = "ok";
+				broadcastMsg(msg);
+
+				// 
+				sleep(10);
+				m_state = 300;
+			}
+			break;
+		}
+		// 3rd section
+		case 300: {
+			m_time = rotateTowardObj(Vector3d(-800,60,-200),m_vel,evt.time());
+			m_state = 301;
+			break;
+		}
+		case 301: {
+			if(evt.time() >= m_time){
+				m_my->setWheelVelocity(0.0, 0.0);
+				m_time = goToObj(Vector3d(-800,60,-200), m_vel*20, 1.0, evt.time());
+				m_state = 302;
+			}
+			break;
+		}
+		case 302: {
+			if(evt.time() >= m_time){
+				m_my->setWheelVelocity(0.0, 0.0);
+				m_time = rotateTowardObj(Vector3d(-970,60,-200),m_vel,evt.time());
+				m_state = 303;
+			}
+			break;
+		}
+		case 303: {
+			if(evt.time() >= m_time){
+				m_my->setWheelVelocity(0.0, 0.0);
+				m_time = goToObj(Vector3d(-970,60,-200), m_vel*20, 1.0, evt.time());
+				m_state = 304;
+			}
+			break;
+		}
+		case 304: {
+			if(evt.time() >= m_time){
+				m_my->setWheelVelocity(0.0, 0.0);
+				m_time = rotateTowardObj(Vector3d(-970,60,-400),m_vel,evt.time());
+				m_state = 305;
+			}
+			break;
+		}
+		case 305: {
+			if(evt.time() >= m_time){
+				m_my->setWheelVelocity(0.0, 0.0);
+				m_time = goToObj(Vector3d(-970,60,-400), m_vel*20, 1.0, evt.time());
+				m_state = 306;
+			}
+			break;
+		}
+		case 306: {
+			if(evt.time() >= m_time){
+				m_my->setWheelVelocity(0.0, 0.0);
+				m_time = rotateTowardObj(Vector3d(-800,60,-550),m_vel,evt.time());
+				m_state = 307;
+			}
+			break;
+		}
+		case 307: {
+			if(evt.time() >= m_time){
+				m_my->setWheelVelocity(0.0, 0.0);
+				m_time = goToObj(Vector3d(-800,60,-550), m_vel*20, 1.0, evt.time());
+				m_state = 900;
+			}
+			break;
+		}
+		// finish line
+		case 900: {
+			if(evt.time() >= m_time){
+				m_my->setWheelVelocity(0.0, 0.0);
+				m_time = rotateTowardObj(Vector3d(-800,60,-750),m_vel,evt.time());
+				m_state = 901;
+			}
+			break;
+		}
+		case 901: {
+			if(evt.time() >= m_time){
+				m_my->setWheelVelocity(0.0, 0.0);
+				m_time = goToObj(Vector3d(-800,60,-750), m_vel*20, 1.0, evt.time());
+				m_state = 999;
+			}
+			break;
+		}
+		case 999: {
+			if(evt.time() >= m_time){
+				m_my->setWheelVelocity(0.0, 0.0);
+				//broadcastMsg("end");
+				m_state = 0;
+			}
+			break;
+		}
+	}
+	return 0.05;
+}
+
+void MyController::onRecvMsg(RecvMsgEvent &evt)
+{
+	std::string msg = evt.getMsg();
+	if(msg == "start"){
+		m_state = 100;
+		broadcastMsg("get message: start");
+	}
+	else if(msg == "end"){
+		//m_my->setWheelVelocity(0.0, 0.0);
+		m_state = 999;
+		broadcastMsg("get message: end");
+	}
+	else if(m_state == 207 && msg == "leave the elevator"){
+		broadcastMsg("get message: leave the elevator");
+		m_state = 208;
+	}
+}
+
+void MyController::onCollision(CollisionEvent &evt) 
+{
+}
+
+double MyController::rotateTowardObj(Vector3d pos, double velocity, double now)
+{
+	// get own position
+	Vector3d myPos;
+	m_my->getPosition(myPos);
+
+	// vector from own position to a target position
+	Vector3d tmpp = pos;
+	tmpp -= myPos;
+
+	// rotation about y-axis is always 0
+	tmpp.y(0);
+
+	// get own rotation
+	Rotation myRot;
+	m_my->getRotation(myRot);
+
+	// initial direction
+	Vector3d iniVec(0.0, 0.0, 1.0);
+
+	// get rotation angle about y-axis
+	double qw = myRot.qw();
+	double qy = myRot.qy();
+	double theta = 2*acos(fabs(qw));
+
+	if(qw*qy < 0){
+		theta = -theta;
+	}
+
+	// angle from z-axis
+	double tmp = tmpp.angle(Vector3d(0.0, 0.0, 1.0));
+	double targetAngle = acos(tmp);
+
+	// calcurate target angle
+	if(tmpp.x() > 0){
+		targetAngle = -1*targetAngle;
+	}
+	targetAngle += theta;
+
+	if(targetAngle<-M_PI){
+		 targetAngle += 2*M_PI;
+	}
+	else if(targetAngle>M_PI){
+		targetAngle -= 2*M_PI;
+	}
+
+	if(targetAngle == 0.0){
+		return 0.0;
+	}
+	else {
+		// circumference length for rotation
+		double distance = m_distance*M_PI*fabs(targetAngle)/(2*M_PI);
+
+		// calcurate velocity from radius of wheels
+		double vel = m_radius*velocity;
+
+		// rotation time (micro second)
+		double time = distance / vel;
+
+		// start rotating
+		if(targetAngle > 0.0){
+			m_my->setWheelVelocity(velocity, -velocity);
+		}
+		else{
+			m_my->setWheelVelocity(-velocity, velocity);
+		}
+
+		return now + time;
+	}
+}
+
+// move to a target point
+double MyController::goToObj(Vector3d pos, double velocity, double range, double now)
+{
+	// get own position
+	Vector3d myPos;
+	m_my->getPosition(myPos);
+
+	// vector from own position to a target position
+	pos -= myPos;
+
+	pos.y(0);
+
+	// distance to a target position
+	double distance = pos.length() - range;
+
+	// calcurate veloocity from radius of wheels
+	double vel = m_radius*velocity;
+
+	// start moving
+	m_my->setWheelVelocity(velocity, velocity);
+
+	// calcurate time of arrival
+	double time = distance / vel;
+
+	return now + time;
+}
+
+extern "C" Controller * createController() {
+	return new MyController;  
+}
+
+
---
+++ FollowMe/WebPage/Multiple/Template_for_RoboCup2014/Operator.cpp
@@ -0,0 +1,398 @@
+#include "ControllerEvent.h"  
+#include "Controller.h"  
+#include "Logger.h"  
+#include <algorithm>
+#include <string> 
+#include <iostream>
+#include <math.h>
+#include <unistd.h>
+
+// Convert degree to radian
+#define DEG2RAD(DEG) ( (M_PI) * (DEG) / 180.0 )
+
+#define WALKING_PERSON  1.0 //人が歩き始める場所
+#define CHECK_POINT1    2.0 //1st Sectionの点数が入る場所
+#define ELEVATOR        3.0 //エレベータ内の待機場所
+#define ELEVATOR_CLEAR  4.0 //2nd Sectionの点数が入る場所
+#define CROWD           5.0 //人ごみを抜けた後の待機場所
+#define CROWD_END       6.0 //3rd Sectionの点数が入る場所
+#define END            99.0 //終了地点
+
+struct coordinate{
+	double x[255];
+	double y[255];
+	double z[255];
+	double flag[255];
+};
+
+char robotName[] = "robot_004";
+
+using namespace std;
+
+class MyController : public Controller {  
+public:
+	void onInit(InitEvent &evt);  
+	double onAction(ActionEvent&);  
+	void onRecvMsg(RecvMsgEvent &evt); 
+	void onCollision(CollisionEvent &evt); 
+
+private:
+	SimObj *my;
+	std::vector<std::string> m_entities;
+
+	coordinate node; 
+
+	bool first;
+	bool start;
+	bool elevator;
+	bool crowd;
+	bool end;
+	bool stop;
+	bool doorClose;
+	bool leaveElevator;
+	//bool flg3;
+	//bool ok;
+	bool elevator_end;
+
+	bool sentMsg_Man;
+	bool sendMsg_CheckPoint1;
+	bool sentMsg_Elevator;
+	bool sentMsg_CheckPoint2;
+	bool sentMsg_Crowd;
+	bool sentMsg_Finishline;
+
+	double dx,dy,dz;
+	int i; 
+
+	bool follow;
+	bool walking;
+
+	FILE* fp;
+	float stepWidth;
+	int sleeptime;
+	const static int SIZE = 30;
+	int motionNum;
+	float HEIGHT[SIZE];
+	float LARM_JOINT1[SIZE]; // left shoulder
+	float LARM_JOINT3[SIZE]; // left elbow
+	float RARM_JOINT1[SIZE]; // right shoulder
+	float RARM_JOINT3[SIZE]; // right elbow
+	float LLEG_JOINT2[SIZE]; // left groin(leg)
+	float LLEG_JOINT4[SIZE]; // left knee
+	float LLEG_JOINT6[SIZE]; // left ankle
+	float RLEG_JOINT2[SIZE]; // right groin(leg)
+	float RLEG_JOINT4[SIZE]; // right knee
+	float RLEG_JOINT6[SIZE]; // right ankle
+
+	int count;
+	int step;
+
+	double interval;
+
+	void initCondition();
+};
+
+void MyController::onInit(InitEvent &evt) 
+{
+	initCondition();
+
+	my = getObj(myname());
+
+	// Put arms down
+	my->setJointAngle("LARM_JOINT2", DEG2RAD(-90));
+	my->setJointAngle("RARM_JOINT2", DEG2RAD(90));
+
+	stepWidth = 85;
+
+	if((fp = fopen("motion.txt", "r")) == NULL) {
+		printf("File do not exist.\n");
+		exit(0);
+	}
+	else{
+		fscanf(fp, "%d", &motionNum);
+		fscanf(fp, "%d", &sleeptime);
+		for(int i=0; i<motionNum; i++){
+			fscanf(fp, "%f %f %f %f %f %f %f %f %f %f %f",
+					   &HEIGHT[i],
+					   &LARM_JOINT1[i],
+					   &LARM_JOINT3[i],
+					   &RARM_JOINT1[i],
+					   &RARM_JOINT3[i],
+					   &LLEG_JOINT2[i],
+					   &LLEG_JOINT4[i],
+					   &LLEG_JOINT6[i],
+					   &RLEG_JOINT2[i],
+					   &RLEG_JOINT4[i],
+					   &RLEG_JOINT6[i]);
+		}
+	}
+
+	interval = sleeptime / 1000000;
+}
+
+void MyController::initCondition()
+{
+	start = false;
+	elevator = false;
+	crowd = false;
+	end = false;
+	stop = true;
+	doorClose = false;
+	//flg3 = false;
+	first = false;
+	leaveElevator=false;
+	elevator_end = false;
+	i=0;
+
+	follow = true;
+	walking = false;
+
+	sentMsg_Man = false;
+	sendMsg_CheckPoint1 = false;
+	sentMsg_Elevator = false;
+	sentMsg_CheckPoint2 = false;
+	sentMsg_Crowd = false;
+	sentMsg_Finishline = false;
+
+	count = 0;
+	step = 0;
+
+	double x=0;
+	double y=0;
+	double z=0;
+	double flag=0; //Check point
+
+	dx=0;
+	dy=0;
+	dz=0;
+
+	if((fp = fopen("node.txt", "r")) == NULL) {
+		LOG_MSG(("File do not exist."));
+		exit(0);
+	}
+	while(fscanf(fp, "%lf,%lf,%lf,%lf", &x,&y,&z,&flag) != EOF) {
+		node.x[i]=x;
+		node.y[i]=y;
+		node.z[i]=z;
+		node.flag[i]=flag;
+		i++;
+	}
+	fclose(fp);
+	i=0;
+
+}
+
+double MyController::onAction(ActionEvent &evt)
+{
+	if(!end && start){
+		
+		Vector3d pos;
+		double angle;
+
+		my->getPosition(pos);
+
+		// 歩いていないとき(node上にいるとき)
+		if(!walking){
+			dx=(node.x[i]-pos.x());
+			dz=(node.z[i]-pos.z());
+			angle = atan2(dx,dz);
+			
+			my->setAxisAndAngle(0,1.0, 0, angle);
+			
+			double r = sqrt(pow(dx,2)+pow(dz,2));
+			
+			step = 2 * (int)r / stepWidth;
+			
+			dx /= step*motionNum;
+			dz /= step*motionNum;
+			
+			walking = true;
+
+			// 
+			/*if(!leaveElevator && !follow && node.flag[i-1]>0){
+				stop = true;
+				LOG_MSG(("wait"));
+			}*/
+		}
+
+		// 歩く
+		if(!stop){
+			double addx = 0.0;
+			double addz = 0.0;
+			
+			if(count%2){
+				for(int j=0; j<motionNum; j++){
+					addx += dx;
+					addz += dz;
+					usleep(sleeptime);
+					my->setPosition(pos.x()+addx, HEIGHT[i], pos.z()+addz);
+					my->setJointAngle("LARM_JOINT1", DEG2RAD(LARM_JOINT1[j]));
+					my->setJointAngle("LARM_JOINT3", DEG2RAD(LARM_JOINT3[j]));
+					my->setJointAngle("RARM_JOINT1", DEG2RAD(RARM_JOINT1[j]));
+					my->setJointAngle("RARM_JOINT3", DEG2RAD(RARM_JOINT3[j]));
+					my->setJointAngle("LLEG_JOINT2", DEG2RAD(LLEG_JOINT2[j]));
+					my->setJointAngle("LLEG_JOINT4", DEG2RAD(LLEG_JOINT4[j]));
+					my->setJointAngle("LLEG_JOINT6", DEG2RAD(LLEG_JOINT6[j]));
+					my->setJointAngle("RLEG_JOINT2", DEG2RAD(RLEG_JOINT2[j]));
+					my->setJointAngle("RLEG_JOINT4", DEG2RAD(RLEG_JOINT4[j]));
+					my->setJointAngle("RLEG_JOINT6", DEG2RAD(RLEG_JOINT6[j]));
+				}
+			}
+			else{
+				for(int j=0; j<motionNum; j++){
+					addx += dx;
+					addz += dz;
+					usleep(sleeptime);
+					my->setPosition(pos.x()+addx, HEIGHT[i], pos.z()+addz);
+					my->setJointAngle("RARM_JOINT1", DEG2RAD(LARM_JOINT1[j]));
+					my->setJointAngle("RARM_JOINT3", DEG2RAD(-LARM_JOINT3[j]));
+					my->setJointAngle("LARM_JOINT1", DEG2RAD(RARM_JOINT1[j]));
+					my->setJointAngle("LARM_JOINT3", DEG2RAD(-RARM_JOINT3[j]));
+					my->setJointAngle("RLEG_JOINT2", DEG2RAD(LLEG_JOINT2[j]));
+					my->setJointAngle("RLEG_JOINT4", DEG2RAD(LLEG_JOINT4[j]));
+					my->setJointAngle("RLEG_JOINT6", DEG2RAD(LLEG_JOINT6[j]));
+					my->setJointAngle("LLEG_JOINT2", DEG2RAD(RLEG_JOINT2[j]));
+					my->setJointAngle("LLEG_JOINT4", DEG2RAD(RLEG_JOINT4[j]));
+					my->setJointAngle("LLEG_JOINT6", DEG2RAD(RLEG_JOINT6[j]));
+				}
+			}
+			count++;
+
+			// 目標点に到着
+			if(step==count){
+				// チェックポイントなら停止
+				if(node.flag[i]){
+					stop = true;
+				}
+				count = 0;
+				step = 0;
+				walking = false;
+				i++;
+			}
+		}
+
+		//LOG_MSG(("%lf",checkPoint));
+
+		/*if(follow && !checkPoint){
+			stop = false;
+		}*/
+
+		double checkPoint = node.flag[i-1];
+
+		if(follow && !elevator){
+			// 人が歩き始める
+			if(checkPoint == WALKING_PERSON && !sentMsg_Man){
+				sendMsg("man_001", "point1");
+				LOG_MSG(("point1"));
+				sentMsg_Man = true;
+			}
+			// チェックポイント1
+			else if(checkPoint == CHECK_POINT1 && !sendMsg_CheckPoint1){
+				sendMsg("score","check1");
+				LOG_MSG(("check point 1"));
+				sendMsg_CheckPoint1 = true;
+			}
+			// 人ごみを通過
+			else if(checkPoint == CROWD_END && !sentMsg_Crowd){
+				std::string msg5 = "crowd";
+				sendMsg("score", msg5);
+				LOG_MSG(("crowd"));
+				sentMsg_Crowd = true;
+			}
+
+			// チェックポイント以外
+			//else if(!checkPoint){
+				stop = false;
+			//}
+		}
+
+		// エレベータ奥
+		if(checkPoint == ELEVATOR && !elevator){
+			if(!leaveElevator){
+				stop = true;
+				elevator = true;
+			}
+		}
+
+		// エレベータ＆ドアを閉めてＯＫ(ロボットが入り終わった)
+		if(elevator && doorClose && !sentMsg_Elevator){
+			sendMsg("wall_008", "elevator_close");
+			LOG_MSG(("elevator close"));
+			sentMsg_Elevator = true;
+		}
+
+		// ロボットが退出したらエレベータから出る
+		if(leaveElevator && !elevator_end){
+			elevator = false;
+			stop = false;
+			elevator_end = true;
+		}
+
+		// エレベータクリア
+		if(checkPoint==ELEVATOR_CLEAR && !sentMsg_CheckPoint2){
+			leaveElevator = false;
+			sendMsg("score","elevator");
+			LOG_MSG(("elevator_clear"));
+			sentMsg_CheckPoint2 = true;
+		}
+
+		// 終了点に着いた
+		if(checkPoint==END && follow){
+			end = true;
+			broadcastMsg("end");
+		}
+	}
+
+	/*// タスクの終了
+	if(node.flag[i-1] == END && follow){
+		broadcastMsg("end");
+	}*/
+
+	return interval;
+}
+
+void MyController::onRecvMsg(RecvMsgEvent &evt)
+{
+	string msg = evt.getMsg();
+
+	// タスク開始
+	if(msg == "start"){
+		initCondition();
+		start = true;
+		stop = false;
+	}
+	// ロボットがエレベータに入り終わった
+	else if(msg == "entered"){
+		LOG_MSG(("entered"));
+		doorClose = true;
+	}
+	// エレベータのドアが開いた
+	else if(msg == "elevator_open"){
+		//sendMsg(robotName,"leave the elevator");
+		broadcastMsg("leave the elevator");
+	}
+	// ロボットがエレベータから退出した
+	else if(msg == "ok"){
+		leaveElevator = true;
+		sendMsg("score","elevator_clear");
+	}
+
+	// 追跡できているか
+	if(msg == "NotFollowing"){
+		follow = false;
+	}
+	else if(msg == "Following"){
+		follow = true;
+	}
+}
+
+void MyController::onCollision(CollisionEvent &evt)
+{
+
+}
+
+extern "C" Controller * createController() {
+	return new MyController;  
+}
+
+
