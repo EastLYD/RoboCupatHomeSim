a1071ebe7619f27987c6aec2a847906427188dfc
HideyukiWada <e14m5228@soka-u.jp>
2015-05-23 18:57:47 +0900

    Some bug fix and improvement for referee service.  - Changed to be not reduce penalty when use special functions.  - Added function to automatically scroll comment list and score list.  - Added function to record received message to log.csv.     Description items is as below.     time,sender,message

--- FollowMe/RoboCup/Referee/RoboCupReferee/RoboCupReferee.h
+++ FollowMe/RoboCup/Referee/RoboCupReferee/RoboCupReferee.h
@@ -11,6 +11,7 @@ public ref class Referee : public sigverse::SIGService
 public:
 	Referee(System::String^ name) : SIGService(name)
 	{
+		remainingTime = "";
 		tmp_score = gcnew System::Collections::Generic::List<int>();
 		tmp_msg  = gcnew System::Collections::Generic::List<System::String^>();
 		m_total = 0;
@@ -18,6 +19,8 @@ public:
 		tmp_total = 0;
 		trialCount = 0;
 		numberOfRepetition = 0;
+		penalty = 0;
+		writeLog = true;
 	};
 	~Referee();
 	int getScore();
@@ -27,6 +30,7 @@ public:
 	int getTotal();
 	int getTrialCount();
 	int getNumberOfRepetition();
+	int getPenalty();
 	void setTotal(int total){ m_total = total; }
 	System::String^ getMessage();
 	System::String^ getRemainingTime();
@@ -37,13 +41,14 @@ public:
 	int tmp_total;
 	int trialCount;
 	int numberOfRepetition;
+	int penalty;
 	
 private:
 	System::Collections::Generic::List<int>^ tmp_score;
 	System::Collections::Generic::List<System::String^>^ tmp_msg;
 	System::String^ remainingTime;
-	FILE *fp;
-	char *msg;
+	std::string sysString2stdStrng(System::String^ sys_str);
+	bool writeLog;
 };
 
   
@@ -51,6 +56,16 @@ Referee::~Referee()
 {  
   this->disconnect();  
 }  
+
+std::string Referee::sysString2stdStrng(System::String^ sys_str)
+{
+	std::string tmp;
+	System::IntPtr mptr;
+	mptr = System::Runtime::InteropServices::Marshal::StringToHGlobalAnsi(sys_str);
+	tmp = static_cast<const char*>(mptr.ToPointer());
+	System::Runtime::InteropServices::Marshal::FreeHGlobal(mptr);
+	return tmp;
+}
 
 int Referee::getScore()
 {
@@ -62,10 +77,8 @@ int Referee::getScore()
 
 int Referee::getTotal()
 {
-	if (tmp_total > 0){
-		return m_total + tmp_total;
-	}
 	return m_total;
+	
 }
 
 int Referee::getTmpTotal()
@@ -80,6 +93,10 @@ int Referee::getTrialCount()
 int Referee::getNumberOfRepetition()
 {
 	return numberOfRepetition;
+}
+int Referee::getPenalty()
+{
+	return penalty;
 }
 System::String^ Referee::getMessage()
 {
@@ -124,80 +141,55 @@ void Referee::onRecvMsg(sigverse::RecvMsgEvent ^evt)
 		// name
 		if(split_msg[1] == "time"){
 			remainingTime = split_msg[2];
-
-			std::string tmp1;
-			System::IntPtr mptr1 = System::Runtime::InteropServices::Marshal::StringToHGlobalAnsi(split_msg[1]);
-			tmp1 = static_cast<const char*>(mptr1.ToPointer());
-			System::Runtime::InteropServices::Marshal::FreeHGlobal(mptr1);
-			
-			std::string tmp2;
-			System::IntPtr mptr2 = System::Runtime::InteropServices::Marshal::StringToHGlobalAnsi(split_msg[2]);
-			tmp2 = static_cast<const char*>(mptr2.ToPointer());
-			System::Runtime::InteropServices::Marshal::FreeHGlobal(mptr2);
-			ofs << tmp1.c_str() << "," << tmp2.c_str() << std::endl;
 		}
 		else if (split_msg[1] == "start"){
 			tmp_msg->Add(split_msg[1]);
-			std::string tmp1;
-			System::IntPtr mptr1 = System::Runtime::InteropServices::Marshal::StringToHGlobalAnsi(split_msg[1]);
-			tmp1 = static_cast<const char*>(mptr1.ToPointer());
-			System::Runtime::InteropServices::Marshal::FreeHGlobal(mptr1);
-
-			ofs << tmp1.c_str() << std::endl;
 		}
 		else if (split_msg[1] == "end"){
 			tmp_msg->Add(split_msg[1]);
-			std::string tmp1;
-			System::IntPtr mptr1 = System::Runtime::InteropServices::Marshal::StringToHGlobalAnsi(split_msg[1]);
-			tmp1 = static_cast<const char*>(mptr1.ToPointer());
-			System::Runtime::InteropServices::Marshal::FreeHGlobal(mptr1);
-
-			ofs << tmp1.c_str() << std::endl;
-
-			if (tmp_total > 0){
-				m_total += tmp_total;
+			int score = tmp_total + penalty;
+			if (score > 0){
+				m_total += score;
 			}
 			tmp_total = 0;
+			penalty = 0;
 		}
 		else if (split_msg[1] == "trial"){
 			trialCount = int::Parse(split_msg[2]);
 			numberOfRepetition = int::Parse(split_msg[3]);
-
-			std::string tmp1;
-			System::IntPtr mptr1 = System::Runtime::InteropServices::Marshal::StringToHGlobalAnsi(split_msg[1]);
-			tmp1 = static_cast<const char*>(mptr1.ToPointer());
-			System::Runtime::InteropServices::Marshal::FreeHGlobal(mptr1);
-
-			std::string tmp2;
-			System::IntPtr mptr2 = System::Runtime::InteropServices::Marshal::StringToHGlobalAnsi(split_msg[2]);
-			tmp2 = static_cast<const char*>(mptr2.ToPointer());
-			System::Runtime::InteropServices::Marshal::FreeHGlobal(mptr2);
-
-			std::string tmp3;
-			System::IntPtr mptr3 = System::Runtime::InteropServices::Marshal::StringToHGlobalAnsi(split_msg[3]);
-			tmp3 = static_cast<const char*>(mptr3.ToPointer());
-			System::Runtime::InteropServices::Marshal::FreeHGlobal(mptr3);
-			ofs << tmp1.c_str() << "," << tmp2.c_str() << "," << tmp3.c_str() << std::endl;
-
 		}
 		else{
-			tmp_msg->Add(split_msg[1]);
-		
+			tmp_msg->Add(split_msg[1]);		
 			// score
 			int score = int::Parse(split_msg[2]);
 			tmp_score->Add(score);
-			tmp_total += score;
-
-			std::string tmp1;
-			System::IntPtr mptr1 = System::Runtime::InteropServices::Marshal::StringToHGlobalAnsi(split_msg[1]);
-			tmp1 = static_cast<const char*>(mptr1.ToPointer());
-			System::Runtime::InteropServices::Marshal::FreeHGlobal(mptr1);
-
-			std::string tmp2;
-			System::IntPtr mptr2 = System::Runtime::InteropServices::Marshal::StringToHGlobalAnsi(split_msg[2]);
-			tmp2 = static_cast<const char*>(mptr2.ToPointer());
-			System::Runtime::InteropServices::Marshal::FreeHGlobal(mptr2);
-			ofs << tmp1.c_str() << "," << tmp2.c_str() << std::endl;
+			if (score >= 0){
+				tmp_total += score;
+			}
+			else{
+				penalty += score;
+			}
 		}
+	}
+	writeLog = true;
+	if (split_msg[0] == "RoboCupReferee"){
+		if (split_msg[1] == "time"){
+			writeLog = false;
+		}
+	}
+	if (writeLog == true){
+		std::string tmp = sysString2stdStrng(remainingTime);
+		ofs << tmp.c_str() << ",";
+
+		tmp = sysString2stdStrng(evt->getSender());
+		ofs << tmp.c_str() << ",";
+
+		array<System::String^>^ split_msg = msg->Split(SepString, System::StringSplitOptions::None);
+		//sendMsg("SIGViewer", split_msg->Length.ToString());
+		for (int i = 0; i < split_msg->Length; i++){
+			tmp = sysString2stdStrng(split_msg[i]);
+			ofs << tmp.c_str() << ",";
+		}
+		ofs << "\n";
 	}
 }
