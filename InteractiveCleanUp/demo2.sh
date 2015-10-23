652f325b1e6b237e77c2dfd8f1811ff1eb8bb136
Tetsunari Inamura <inamura@nii.ac.jp>
2015-10-23 21:59:14 +0900

    Remove proper directory name

--- InteractiveCleanUp/demo2.sh
+++ InteractiveCleanUp/demo2.sh
@@ -6,9 +6,9 @@ then
 else
     LD_LIBRARY_PATH=/usr/lib/jvm/java-6-openjdk-i386/jre/lib/i386/server:$LD_LIBRARY_PATH
 fi
-SIGVERSE_X3DPARSER_CONFIG=/home/inamura/sigverse-2.2.2/share/sigverse/etc/X3DParser.cfg
-SIGVERSE_RUNAC=/home/inamura/sigverse-2.2.2/bin/sigrunac
-SIGVERSE_DATADIR=/home/inamura/sigverse-2.2.2/share/sigverse/data
+SIGVERSE_X3DPARSER_CONFIG=$SIGVERSE_PATH/share/sigverse/etc/X3DParser.cfg
+SIGVERSE_RUNAC=$SIGVERSE_PATH/bin/sigrunac
+SIGVERSE_DATADIR=$SIGVERSE_PATH/share/sigverse/data
 export LD_LIBRARY_PATH SIGVERSE_X3DPARSER_CONFIG SIGVERSE_RUNAC SIGVERSE_DATADIR
 
 /home/inamura/sigverse-2.2.2/bin/sigserver -w ./CleanUpTest2.xml
