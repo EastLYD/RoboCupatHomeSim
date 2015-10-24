e6e8f9ee399d3e454cc44ded7ce3f47a37717714
Tetsunari Inamura <inamura@nii.ac.jp>
2015-10-24 20:03:48 +0900

    Delete proper name in configuration

--- InteractiveCleanUp/demo.sh
+++ InteractiveCleanUp/demo.sh
@@ -1,11 +1,5 @@
 #!/bin/sh -x
 
-if [ -z $LD_LIBRARY_PATH ]
-then
-    LD_LIBRARY_PATH=/usr/lib/jvm/java-6-openjdk-i386/jre/lib/i386/server
-else
-    LD_LIBRARY_PATH=/usr/lib/jvm/java-6-openjdk-i386/jre/lib/i386/server:$LD_LIBRARY_PATH
-fi
 SIGVERSE_X3DPARSER_CONFIG=$SIGVERSE_PATH/share/sigverse/etc/X3DParser.cfg
 SIGVERSE_RUNAC=$SIGVERSE_PATH/bin/sigrunac
 SIGVERSE_DATADIR=$SIGVERSE_PATH/share/sigverse/data
